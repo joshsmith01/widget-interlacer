096dd67898b8f0999bed1fb24c6333ea6d96e1d4
Joshua Smith <joshsmith01@me.com>
2018-01-28 11:23:54 -0800

    Remove calls to enqueue scripts
    This version doesn't need to use any javascript

--- widget-interlacer.php
+++ widget-interlacer.php
@@ -18,19 +18,3 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 require_once( 'includes/interlace-content.php' );
 require_once( 'includes/register-interlace-sidebar.php' );
 require_once( 'includes/interlace-post-metabox-options.php' );
-
-
-function widget_interlacer_enqueue_plugin_styles_scripts() {
-	// Set the file path to a variable. -JMS
-	$file_path = plugin_dir_path( __DIR__ ) . 'widget-interlacer.php';
-	// Read the version number from the main plugin file then set it to a variable. -JMS
-	$plugin_data = get_file_data( $file_path, array(
-		'Version' => 'Version'
-	) );
-	// The the value of the Version header to a variable. -JMS
-	$ver = $plugin_data['Version'];
-
-	// Use the variable, $ver, in more than one stylesheet/script to bust cache when the plugin gets updated. -JMS
-	wp_enqueue_script( 'widget-interlacer-public-js', plugins_url( 'public/js/widget-interlacer-public.js', __FILE__ ), array(), $ver, true );
-}
-add_action( 'wp_enqueue_scripts', 'widget_interlacer_enqueue_plugin_styles_scripts' );
